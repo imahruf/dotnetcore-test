a7dfe71a23946ce9249d55e5ce9bf710e56af469
imahruf <53860066+imahruf@users.noreply.github.com>
2019-10-16 14:45:43 +0100

    t

--- tests/StringUtilsTests.cs
+++ tests/StringUtilsTests.cs
@@ -13,7 +13,7 @@ namespace Tests
         public void ReverseString()
         {
             var inputString = "The quick brown fox jumps over the lazy dog";
-            var expectedString = "god yzal eht revo spmuj xof nworb kciuq eh";
+            var expectedString = "god yzal eht revo spmuj xof nworb kciuq ehT";
             var returnedString = StringUtils.ReverseString(inputString);
             Assert.True(expectedString == returnedString, "The input string was not reversed correctly.");
         }
